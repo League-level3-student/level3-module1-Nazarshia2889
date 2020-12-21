d9834f931b4536fc769a2ef56675f87a9aa06f1b
Nazarshia2889 <arshia.nayebnazar@yahoo.com>
2020-12-20 16:43:16 -0800

    coolio

--- src/_04_HangMan/HangMan.java
+++ src/_04_HangMan/HangMan.java
@@ -11,15 +11,19 @@ import javax.swing.JPanel;
 
 public class HangMan implements KeyListener{
 	
-	Utilities u = new Utilities();
-	Stack<String> words = new Stack<String>();
 	
-	JFrame frame = new JFrame();
-	JPanel panel = new JPanel();
-	JLabel label = new JLabel();
+	Stack<String> words; 
+	
+	JFrame frame; 
+	JPanel panel;
+	JLabel label; 
+	JLabel livesNumber;
+	
 	
 	String current;
-	 
+	int lives = 10;
+	
+	
 	
 	public static void main(String[] args) {
 		HangMan hm = new HangMan();
@@ -27,12 +31,18 @@ public class HangMan implements KeyListener{
 	}
 	
 	void run() {
+		words = new Stack<String>();
+		frame = new JFrame();
+		panel = new JPanel();
+		label = new JLabel();
+		livesNumber = new JLabel();
+		
 		String numberOfWords = JOptionPane.showInputDialog(null, "How many words do you want to play with.");
 		int number = Integer.parseInt(numberOfWords);
 		for(int i = 0; i<number; i++) {
-			String x = u.readRandomLineFromFile("dictionary.txt");
+			String x = Utilities.readRandomLineFromFile("dictionary.txt");
 			if(words.contains(x)) {
-				String y = u.readRandomLineFromFile("dictionary.txt");
+				String y = Utilities.readRandomLineFromFile("dictionary.txt");
 				words.push(y);
 			}
 			else {
@@ -47,6 +57,7 @@ public class HangMan implements KeyListener{
 		frame.addKeyListener(this);
 		
 		panel.add(label);
+		panel.add(livesNumber);
 		
 		current = words.pop();
 		String lines = "";
@@ -54,6 +65,11 @@ public class HangMan implements KeyListener{
 			lines += "_";
 			label.setText(lines);
 		}
+		
+		livesNumber.setText("Lives: " + String.valueOf(lives));
+		
+		
+		
 		
 		frame.pack();
 	}
@@ -68,6 +84,7 @@ public class HangMan implements KeyListener{
 	@Override
 	public void keyPressed(KeyEvent e) {
 		// TODO Auto-generated method stub
+		boolean found = false;
 		
 		char c = e.getKeyChar();
 		
@@ -75,16 +92,60 @@ public class HangMan implements KeyListener{
 		char[] answer = x.toCharArray();
 		
 		char[] characters = current.toCharArray(); 
+	
 		
 		for(int i = 0;i<characters.length;i++) {
+			
 			if(characters[i] == c) {
-				answer[i] = c; 
-				String result = new String(answer);
-				label.setText(result);
+				if(answer[i] == c) {
+					continue;
+				}
+				else {
+					answer[i] = c; 
+					String result = new String(answer);
+					label.setText(result);
+					livesNumber.setText("Lives: " + String.valueOf(lives));
+					found = true;
+					int count = 0;
+					for(int j = 0;j<answer.length;j++) {
+						if(answer[j] == '_') {
+							count += 1;
+						}
+					}
+					if(count == 0) {
+						label.removeAll();
+						current = words.pop();
+						String lines = "";
+						for(int m = 0; m<current.length();m++) {
+							lines += "_";
+							label.setText(lines);
+						}
+					}
+					break;
+				}
 			}
 		}
+		if(found == false) {
+			lives -= 1;
+			livesNumber.setText("Lives: " + String.valueOf(lives));
+			if(lives == 0) {
+				String replay = JOptionPane.showInputDialog(null, "Do you want to play again? Please put Yes or No: ");
+				if(replay.equalsIgnoreCase("Yes")) {
+					frame.dispose();
+					lives = 10;
+					HangMan hm = new HangMan();
+					hm.run();
+				}
+				else {
+					System.exit(1);
+				}
+				}
+			}
+		}
+	
 		
-	}
+		
+	
 
 	@Override
 	public void keyReleased(KeyEvent e) {
